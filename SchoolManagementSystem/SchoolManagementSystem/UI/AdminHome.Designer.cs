729ca4b89686009c075e5bba9fa7c6b6537e1d3a
TahirRajpoot <ranat5177@gmail.com>
2023-12-09 10:33:13 +0500

    Add Admin Dashboard #5

---
+++ SchoolManagementSystem/SchoolManagementSystem/UI/AdminHome.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace SchoolManagementSystem.UI
+{
+    partial class AdminHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AdminHome));
+            this.materialTabControl1 = new MaterialSkin.Controls.MaterialTabControl();
+            this.Home = new System.Windows.Forms.TabPage();
+            this.Teachers = new System.Windows.Forms.TabPage();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.materialTextBox1 = new MaterialSkin.Controls.MaterialTextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.materialTabControl1.SuspendLayout();
+            this.Home.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // materialTabControl1
+            // 
+            this.materialTabControl1.Controls.Add(this.Home);
+            this.materialTabControl1.Controls.Add(this.Teachers);
+            this.materialTabControl1.Depth = 0;
+            this.materialTabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.materialTabControl1.ImageList = this.imageList1;
+            this.materialTabControl1.Location = new System.Drawing.Point(3, 64);
+            this.materialTabControl1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabControl1.Multiline = true;
+            this.materialTabControl1.Name = "materialTabControl1";
+            this.materialTabControl1.SelectedIndex = 0;
+            this.materialTabControl1.Size = new System.Drawing.Size(824, 418);
+            this.materialTabControl1.TabIndex = 0;
+            // 
+            // Home
+            // 
+            this.Home.Controls.Add(this.tableLayoutPanel1);
+            this.Home.ImageIndex = 1;
+            this.Home.Location = new System.Drawing.Point(4, 23);
+            this.Home.Name = "Home";
+            this.Home.Padding = new System.Windows.Forms.Padding(3);
+            this.Home.Size = new System.Drawing.Size(816, 391);
+            this.Home.TabIndex = 0;
+            this.Home.Text = "Home";
+            this.Home.UseVisualStyleBackColor = true;
+            // 
+            // Teachers
+            // 
+            this.Teachers.ImageKey = "icons8-home-32.png";
+            this.Teachers.Location = new System.Drawing.Point(4, 23);
+            this.Teachers.Name = "Teachers";
+            this.Teachers.Padding = new System.Windows.Forms.Padding(3);
+            this.Teachers.Size = new System.Drawing.Size(786, 356);
+            this.Teachers.TabIndex = 1;
+            this.Teachers.Text = "Teachers";
+            this.Teachers.UseVisualStyleBackColor = true;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "icons8-home-32.png");
+            this.imageList1.Images.SetKeyName(1, "icons8-people-32.png");
+            this.imageList1.Images.SetKeyName(2, "icons8-apps-tab-32 (1).png");
+            this.imageList1.Images.SetKeyName(3, "icons8-edit-24.png");
+            this.imageList1.Images.SetKeyName(4, "icons8-delete-24.png");
+            this.imageList1.Images.SetKeyName(5, "icons8-edit-account-48.png");
+            this.imageList1.Images.SetKeyName(6, "icons8-dashboard-32 (1).png");
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.materialTextBox1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 57.29167F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 42.70833F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 49F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 239F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(810, 385);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // materialTextBox1
+            // 
+            this.materialTextBox1.AnimateReadOnly = false;
+            this.materialTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.materialTextBox1.Depth = 0;
+            this.materialTextBox1.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.materialTextBox1.LeadingIcon = null;
+            this.materialTextBox1.Location = new System.Drawing.Point(3, 3);
+            this.materialTextBox1.MaxLength = 50;
+            this.materialTextBox1.MouseState = MaterialSkin.MouseState.OUT;
+            this.materialTextBox1.Multiline = false;
+            this.materialTextBox1.Name = "materialTextBox1";
+            this.materialTextBox1.Size = new System.Drawing.Size(142, 50);
+            this.materialTextBox1.TabIndex = 0;
+            this.materialTextBox1.Text = "";
+            this.materialTextBox1.TrailingIcon = null;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.RosyBrown;
+            this.panel1.Location = new System.Drawing.Point(3, 58);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 35);
+            this.panel1.TabIndex = 1;
+            // 
+            // AdminHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(830, 485);
+            this.Controls.Add(this.materialTabControl1);
+            this.DrawerTabControl = this.materialTabControl1;
+            this.Name = "AdminHome";
+            this.Text = "AdminHome";
+            this.Load += new System.EventHandler(this.AdminHome_Load);
+            this.materialTabControl1.ResumeLayout(false);
+            this.Home.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl materialTabControl1;
+        private System.Windows.Forms.TabPage Home;
+        private System.Windows.Forms.TabPage Teachers;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private MaterialSkin.Controls.MaterialTextBox materialTextBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ SchoolManagementSystem/SchoolManagementSystem/UI/AdminHome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using MaterialSkin;
+using MaterialSkin.Controls;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SchoolManagementSystem.UI
+{
+    public partial class AdminHome : MaterialForm
+    {
+        private readonly MaterialSkinManager materialSkinManager = null;
+        public AdminHome()
+        {
+            InitializeComponent();
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+          
+        }
+
+        private void AdminHome_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
